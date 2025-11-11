8bbf14fc4513560e4d04300ac0ae44ebab67c938
KostyaKaly <ho1189h@gmail.com>
2025-11-12 00:49:18 +0200

    Update Kostya.py

--- Kostya.py
+++ Kostya.py
@@ -1,1 +1,20 @@
-
+name="Kostya"
+age=17
+km=12.4
+my_favorite_games=[GTA5, FC26]
+my_shortcomings=[]
+student=true
+schoolboy=false
+shopping=(bananas, meat, bread)
+my_favorite_cars={CLS63amg, AudiRS6, PORSHE911}
+pets=["1": "dog", "2": "cat" }
+print (name, type (name ))
+print (age, type (age))
+print (km, type (km))
+print(my_favorite_games, type (my_favorite_games))
+print(my_shortcomings, type (my_shortcoming))
+print(student, type (student))
+print(schoolboy, type (schoolboy))
+print(shopping, type(sopping))
+print(my_favorite_cars, type (my_favorite_cars))
+print(pets, type(pets))
